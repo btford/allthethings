5cd609dd734848fc4d930c96241ac36c20f8fd1f
Brian Ford <btford@btfdesktop.(none)>
2012-02-04 00:59:44 -0500

    Initial commit

---
+++ allthethings.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# All the Things ASCII Art v0.0.1
+# by @briantford
+# https://github.com/btford/allthethings
+#
+# Licensed under WTFPL http://sam.zoy.org/wtfpl/
+#
+
+
+pur=$(tput setaf 5)    # Purple
+ylw=$(tput setaf 3)    # Yellow
+txtrst=$(tput sgr0)       # Text reset
+
+echo " _   _ ___ ___   _ _____ ___     _   _    _      _____ _  _ ___ "
+echo "| | | | _ \   \ /_\_   _| __|   /_\ | |  | |    |_   _| || | __|"
+echo "| |_| |  _/ |) / _ \| | | _|   / _ \| |__| |__    | | | __ | _| "
+echo " \___/|_| |___/_/ \_\_| |___| /_/ \_\____|____|   |_| |_||_|___|"
+echo "                             =================="                                 
+echo " _____ _  _ ___ _  _  ___ ___ "
+echo "|_   _| || |_ _| \| |/ __/ __|"
+echo "  | | | __ || ||    | (_ \__ \\"
+echo "  |_| |_||_|___|_|\_|\___|___/"
+
+
+echo "                  ${ylw}~=                                                 ${txtrst}M?${ylw}              ,=~${txtrst}"
+echo "                   ${ylw}==                                                ${txtrst}MD${ylw}           ~=~=~${txtrst}"
+echo "                   ${ylw}====                                               ${txtrst}+${ylw}        :=~===${txtrst}"
+echo "                   ${ylw}~====~                        ~                    ${txtrst}M${ylw}     :~======${txtrst}"
+echo "                    ${ylw}~=====~           ~         ==               ~==  ${txtrst}M${ylw}   ~========${txtrst}"
+echo "                     ${ylw}~====~~~        ,==       ~==            ,====   ${txtrst}Z${ylw}~~=========${txtrst}"
+echo "                     ${ylw}=======~=~      ~===,     ===~          =====   ${txtrst}=${ylw}==========~${txtrst}"
+echo "                     ${ylw},===========:   ~=====   :===~       ,======:===${txtrst}~${ylw}~=======~~${txtrst}"
+echo "                      ${ylw}=~===========  ~======  ~====     ,~===========${txtrst}+${ylw}========~${txtrst}"
+echo "                      ${ylw},~=============~=======~===~=~  ~==============${txtrst}M${ylw}=======~${txtrst}"
+echo "                       ${ylw}=============~~======${txtrst}?MNDDM${ylw}~~==========~======${txtrst}M${ylw}~=====~${txtrst}"
+echo "                        ${ylw}================${txtrst}M=,,,,,,,,,D${ylw}~======${txtrst}M   N${ylw}=====${txtrst}N${ylw}=====~${txtrst}"
+echo "         ${ylw}==~            =================~${txtrst}N~,,,,,,,:M8MMMNM   O M====${txtrst}~${ylw}=====${txtrst}"
+echo "          ${ylw},===~,         ~=================~~${txtrst}MM~          M     M====${txtrst}=${ylw}==== ${txtrst}"
+echo "            ${ylw}~===~=       :=================~${txtrst}N             OM   M ===${txtrst}?${ylw}~=== ${txtrst}"
+echo "              ${ylw}~====~~     ==============${txtrst}M   M     ?M ~   8Z      ~:=${txtrst}N${ylw}~==:${txtrst}"
+echo "    M          ${ylw}~======~=~  ============${txtrst}M O     ?M +7MMMMMMMI      8=${txtrst}M${ylw}~==${txtrst}"
+echo "   NO:7:        ${ylw}:======================${txtrst}7    M=  MNMMMMMMMMMM       +${txtrst}N${ylw}==${txtrst}"
+echo "MMO:::::?         ${ylw}=======================${txtrst}8M~M ~MMMMMMMMMMMMMM      M+${ylw}~~                     ,===~${txtrst}"
+echo "M,::::::::N        ${ylw}=======================${txtrst}N  MMMMMMMMMMMMMMMM      M~${ylw}=    :=~~=~         ~=~~==~${txtrst}"
+echo ":,:::::::::Z         ${ylw}=====================${txtrst}7    MMMMMMMMMMMMMM      M${ylw}===~=====~       ,=~~====${txtrst}"
+echo "?:::::::::::M    ${ylw}==========================${txtrst}M    ~MMMMMMMMMMMM     II${ylw}========      :~=======${txtrst}"
+echo " ,::::::::::,     ${ylw},========================~${txtrst}8     MMMMMMMMMMI     M${pur}?M${ylw}======    =======~==${txtrst}"
+echo " 8:::::::::::N      ${ylw}===~====================+${txtrst}N    MMMMMMMMMM      I${pur}?I${ylw}===~= ~==========~${txtrst}"
+echo "   D:::::::::         ${ylw}======================${txtrst}MM    MMMMMMMMM ,    I${pur}???M${ylw}==~===========${txtrst}"
+echo "    N~==OMMZZ7         ${ylw}:================~+${txtrst}M${ylw}===${txtrst}M    8::~,  =N    M${pur}????D${ylw}============~${txtrst}"
+echo "           OZZM     ${ylw}~===============~==${txtrst}M${ylw}~~=====${txtrst}M               O${pur}??????${ylw}===========${txtrst}"
+echo "           ,MZZM=${ylw}~==================${txtrst}OO${ylw}=~========${txtrst}M~           DD${pur}???????M${ylw}========~${txtrst}"
+echo "            ~MZZZ?${ylw}===============${txtrst}MM${ylw}=~===========~${txtrst}7?NMMZZNNM7${pur}?I????????7${ylw}~======${txtrst}"
+echo "    ${ylw}~~~~,     ${txtrst}:ZZZM${ylw}============${txtrst}N+${ylw}================${pur}N?????????????????????O${ylw}====~=~~~~~~===~~:,${txtrst}"
+echo "     ${ylw}=========~,${txtrst}MOZM${ylw}~=======${txtrst}NN${ylw}====================${pur}M????????????????????N${ylw}==========================,${txtrst}"
+echo "      ${ylw}===========${txtrst}MZZO${ylw}=~===${txtrst}M${ylw}~=~====================~${pur}?????????????????????~${ylw}========================~=~${txtrst}"
+echo "       ${ylw}~~=========${txtrst}?ZZZNN8${ylw}==========================${pur}MI???????????????????M${ylw}=====================~~${txtrst}"
+echo "       ${ylw}=============${txtrst}NMZM${ylw}============================${pur}????????????????????O${ylw}=================~${txtrst}"
+echo "        ${ylw}===========~~${txtrst}MZZO${ylw}~==========================${pur}M????????????????????${ylw}============~,${txtrst}"
+echo "         ${ylw}=============${txtrst}OZZZN${ylw}=========================${pur}M????????????????????${ylw}~========,${txtrst}"
+echo "          ${ylw}~============~${txtrst}MZZM${ylw}~~======================${pur}8????????????????????${ylw}~=====${txtrst}"
+echo "           ${ylw}~============~${txtrst}MZZM${ylw}======================~${pur}?????????????????????${ylw}~~===${txtrst}"
